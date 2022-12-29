097ca8b69aa20ad0c6a0db01ad4dd7c0e49ef6a2
Cordelia Payson <cordelia.payson@gmail.com>
2022-12-29 12:05:21 -0500

    adding features to products

---
+++ server/db/featureStream.js
@@ -0,0 +1,52 @@
+import fs from 'fs';
+import es from 'event-stream';
+import csv from 'csv-parser';
+import mongoose from 'mongoose';
+import { addFeatures } from './products.js';
+
+mongoose.connect('mongodb://localhost/sdc');
+
+const streamData = (filePath) => {
+  const cb = () => {
+    console.log('the end');
+  };
+
+  let currentId = 0;
+  let currentFeatures = [];
+
+  const newFeature = (line) => {
+    const feature = {
+      feature: line.feature,
+      value: line.value,
+    };
+    currentFeatures.push(feature);
+  };
+
+  fs
+    .createReadStream(filePath)
+    .pipe(csv())
+    .on('error', (error) => error)
+    .pipe(es.map((line, cb) => {
+      if (currentId === 0) {
+        currentId = line.product_id;
+      }
+      if (currentId !== line.product_id) {
+        addFeatures(currentId, currentFeatures);
+        currentId = line.product_id;
+        currentFeatures = [];
+      }
+      if (currentId === line.product_id) {
+        newFeature(line);
+      }
+      cb();
+    }))
+
+    .on('end', () => {
+      addFeatures(currentId, currentFeatures);
+      console.log('all done');
+    })
+    .on('error', (error) => error);
+};
+
+// streamData('data_samples/featuresSample.csv');
+streamData('data/features.csv');
--- server/db/products.js
+++ server/db/products.js
@@ -2,28 +2,50 @@ import mongoose from 'mongoose';
 
 mongoose.connect('mongodb://localhost/sdc');
 
-const productSchema = mongoose.Schema({
-  _id: Number,
-  name: String,
-  slogan: String,
-  description: String,
-  category: String,
-  default_price: String,
-  created_at: String,
-  updated_at: String,
-  features: [{ type: Number, ref: 'Feature' }],
+const featureSchema = mongoose.Schema({
+  feature: String,
+  value: String,
 });
+
+// const feature = mongoose.model('Feature', featureSchema);
+
+const productSchema = mongoose.Schema(
+  {
+    _id: Number,
+    name: String,
+    slogan: String,
+    description: String,
+    category: String,
+    default_price: String,
+    features: [featureSchema],
+  },
+  { timestamps: true },
+);
 
 const Product = mongoose.model('Product', productSchema);
 
 export const saveManyProduct = (data) => Product.insertMany(data)
   .then(() => {
-    console.log('success');
+    console.log('product success');
   })
   .catch((err) => (err));
 
-  export const editProduct = (product) => {
-    line._id = line.id;
-    console.log(product);
-  };
+// export const editProduct = (product) => {
+//   line._id = line.id;
+//   console.log(product);
+// };
 
+// export const addFeatures = (id, newFeatures) => {
+//   return Product.findByIdAndUpdate(id, { features: newFeatures})
+//     .then(() => {
+//       console.log('features added');
+//     })
+//     .catch((err) => err);
+//   }
+
+export const addFeatures = (id, newFeatures) => Product.findById(id)
+  .then((product) => {
+    product.features = newFeatures;
+    product.save();
+  })
+  .catch((err) => err);
