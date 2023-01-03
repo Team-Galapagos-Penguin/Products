3f1c22dd6b2d6b6dedf03c2a8da4ed5a7bcd9b8e
Cordelia Payson <cordelia.payson@gmail.com>
2023-01-03 12:31:17 -0500

    setting up API routes

--- server/index.js
+++ server/index.js
@@ -10,5 +10,14 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// GET /products/:product_id
+app.get('/products/:product_id', (req, res) => {
+  console.log(req.params);
+});
+
+// GET /products/:product_id/styles
+
+// GET /products/:product_id/related
+
 app.listen(process.env.PORT);
 console.log(`Listening at http://localhost:${process.env.PORT}`);
